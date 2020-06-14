08654ffe628d2fd8793748e13f092b113901ee58
Said-Abdikarim <kareemfrmto@gmail.com>
2020-06-14 14:07:25 -0400

    Config file for the database credentials and phpmail
    Allows the programmer to access the password and username from one area.

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'database' => [
+        'dsn' => "",
+        'user' => '',
+        'password' => '',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+        ],
+
+    'phpmail' => [
+        'host' => "",
+        'user' => '',
+        'pass' => '',
+        'name' => ""
+    ]       
+];

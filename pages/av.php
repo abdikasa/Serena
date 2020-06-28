e2baa48476d4f10d717acb47041ec50c817432a0
Said-Abdikarim <kareemfrmto@gmail.com>
2020-06-28 16:13:23 -0400

    This page is what users see when they are already verified.

---
+++ pages/av.php
@@ -0,0 +1,10 @@
+<?php require_once "../includes/cym_header.php"; ?>
+<img src="/SERENA/imgs/av.png" alt="Email icon">
+<div id="text-cym">
+    <h2>Error: Already Verified</h2>
+    <h2 id="highlight-h2">This email is already verified</h2>
+    <p>
+        You are already eligible for our special deals, gifts, and sneak peeks into our newest and 
+        most greatest collections in <?php echo date("Y"); ?>.
+    </p>
+    <?php require_once "../includes/cym_footer.php"; ?>

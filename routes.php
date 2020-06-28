4dc5774c41e40795f05aa0c668b5ad2875f4bd6f
Said-Abdikarim <kareemfrmto@gmail.com>
2020-06-28 16:12:40 -0400

    Holds the routes

--- routes.php
+++ routes.php
@@ -5,3 +5,4 @@ $router->post("serena", "PageController@home");
 $router->get("serena/register", "PageController@register");
 $router->post("serena/register", "PageController@register");
 $router->post("serena/checkemail", "PageController@checkemail");
+

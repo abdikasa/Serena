f1007f9d8e03272b43910eb16a91d67aeb63c735
Said-Abdikarim <kareemfrmto@gmail.com>
2020-06-28 16:13:05 -0400

    Verifies user successfully if they haven't been verified already.

---
+++ pages/verified.php
@@ -0,0 +1,10 @@
+<?php require_once "../includes/cym_header.php"; ?>
+<img src="/SERENA/imgs/success.png" alt="Email icon">
+<div id="text-cym">
+    <h2>Verification Success</h2>
+    <h2 id="highlight-h2">This email is now verified</h2>
+    <p>
+        You are now eligible for our special deals, gifts, and sneak peeks into our newest and 
+        most greatest collections coming in <?php echo date("Y"); ?>.
+    </p>
+<?php require_once "../includes/cym_footer.php"; ?>
